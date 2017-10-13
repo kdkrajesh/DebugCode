e484981e8089d56568abbda771cb5e3ca5620c12
santoshbista02 <santoshbista02@gmail.com>
2017-10-13 21:06:41 +1100

    Changes in Player .java and Main.java

--- Player.java
+++ Player.java
@@ -3,11 +3,10 @@ public class Player {
 	private String name;
 	private int balance;
 	private int limit;
-	private int bet;
 	
 	public Player(String name, int balance) {
 		if (name == null || name .isEmpty()) throw new IllegalArgumentException("Name cannot be null or empty");
-		if (balance > 100) throw new IllegalArgumentException("Balance cannot be negative");
+		if (balance < 0) throw new IllegalArgumentException("Balance cannot be negative");
 		this.name = name;
 		this.balance = balance;
 		this.limit = 0;
@@ -18,7 +17,7 @@ public class Player {
 	public int getLimit() { return limit; }
 	
 	public void setLimit(int limit) {
-		if (limit > 100) throw new IllegalArgumentException("Limit cannot be negative.");
+		if (limit < 0) throw new IllegalArgumentException("Limit cannot be negative.");
 		if (limit > balance)  throw new IllegalArgumentException("Limit cannot be greater than balance.");
 		this.limit = limit;
 	}
@@ -28,21 +27,18 @@ public class Player {
 	}
 	
 	public boolean balanceExceedsLimitBy(int amount) {
-		return (balance - amount > limit); //Changing the >= sign
+		return (balance - amount > limit);
 	}
 	
 	public void takeBet(int bet) {
 		if (bet < 0) throw new IllegalArgumentException("Bet cannot be negative.");
 		if (!balanceExceedsLimitBy(bet)) throw new IllegalArgumentException("Placing bet would go below limit.");
 		balance = balance - bet;
-		
 	}
 	
 	public void receiveWinnings(int winnings) {
-		
-		if (winnings > 0) throw new IllegalArgumentException("Winnings cannot be negative.");
-		balance = balance + winnings;
-			
+		if (winnings < 0) throw new IllegalArgumentException("Winnings cannot be negative.");
+		balance = balance + winnings;		
 	}
 	
 	public String toString() {
